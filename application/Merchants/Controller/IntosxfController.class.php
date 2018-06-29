90d8ddb686decb65c18b919d93d2a4bc2c0be31e
nuxn <lxl4093@126.com>
2018-06-29 09:18:40 +0800

    随行付进件

--- application/Merchants/Controller/IntosxfController.class.php
+++ application/Merchants/Controller/IntosxfController.class.php
@@ -37,7 +37,7 @@ class IntosxfController extends AdminbaseController
 
         $page = $this->page($count, 20);
         $info = $this->sxfModel
-            ->field('b.id,b.mno,b.merchant_id,b.status,b.add_time,m.merchant_name')
+            ->field('b.id,b.mno,b.task_code,b.merchant_id,b.status,b.add_time,m.merchant_name')
             ->join('b left join ypt_merchants m on b.merchant_id=m.id')
             ->order('b.id desc')
             ->limit($page->firstRow, $page->listRows)
@@ -78,6 +78,9 @@ class IntosxfController extends AdminbaseController
             }
         } else {
             $merchant_id = I('id');
+            if($this->sxfModel->where(array('merchant_id'=>$merchant_id))->getField('id')){
+                redirect(U('index'));
+            }
             $province = $this->get_province();
             $list = M('Merchants')->field('m.id as `商户id`,m.merchant_name as `商户名称`,m.merchant_jiancheng as `商户简称`,u.user_phone as `手机号`,
             header_interior_img as `门头`,interior_img,business_license_number as `营业执照号`,business_license as `营业执照`,province as `省`,city as `市`,county as `区县`,address as `详细地址`,
@@ -114,6 +117,7 @@ class IntosxfController extends AdminbaseController
             }
             $list['法人结算正面'] = $list['法人正面'];
             $list['法人结算反面'] = $list['法人反面'];
+            $list['结算身份证号'] = $list['身份证'];
             $this->assign('img', $img);
             $this->assign('list', $list);
             $this->assign('id', $merchant_id);
